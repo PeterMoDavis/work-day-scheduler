266f78052c2b772a32d957ef5c4ba8f0c219c20a
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-12 18:24:20 -0500

    finally got the correct save button to console log the joined message button

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,24 @@
+let todaysDateEl = $("#currentDay");
+let container = $(".container");
+let textArea = $("textarea");
 
+let workDay = {
+  9: "",
+  10: "",
+  11: "",
+  12: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+};
+
+container.on("click", "button", function (e) {
+  e.preventDefault();
+  let name = $(this).attr("name");
+  let correctTextArea = $(`textarea[name = "${name}"]`);
+  console.log(correctTextArea.val());
+});
+
+todaysDateEl.text(moment().format("dddd MMMM Do"));
