88176d0e3b5b8601a1075f626369b883b6865a23
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-13 15:57:30 -0500

    got the local storage to save inputs in correct order

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,7 +2,7 @@ let todaysDateEl = $("#currentDay");
 let container = $(".container");
 let textArea = $("textarea");
 
-let workDay = {
+let todos = {
   9: "",
   10: "",
   11: "",
@@ -16,9 +16,35 @@ let workDay = {
 
 container.on("click", "button", function (e) {
   e.preventDefault();
+  //get name of the button
   let name = $(this).attr("name");
+  console.log(name);
+  //connect button with text area of the same name
   let correctTextArea = $(`textarea[name = "${name}"]`);
-  console.log(correctTextArea.val());
+  //set storage
+  //get the todos object out of storage
+  let storagedTodos = JSON.parse(localStorage.getItem("todos"));
+  //add todo to the properly number key in the todos object
+  storagedTodos[name] = correctTextArea.val();
+
+  //loop through todos object
+  for (let todo in storagedTodos) {
+    //get key value then get text area
+    correctTextArea.text(storagedTodos[todo]);
+  }
+  localStorage.setItem("todos", JSON.stringify(storagedTodos));
 });
 
 todaysDateEl.text(moment().format("dddd MMMM Do"));
+
+function getStorage() {
+  //get local storage
+  let storagedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  for (let todo in storagedTodos) {
+    let correctTextArea = $(`textArea[name="${todo}"`);
+    correctTextArea.text(storagedTodos[todo]);
+  }
+}
+
+getStorage();
