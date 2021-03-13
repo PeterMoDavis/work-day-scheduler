981df603eb390178ac2cede1fdaf64f703d239ac
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-13 17:57:57 -0500

    worked out the logic for the colorized textareas

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,18 +2,7 @@ let todaysDateEl = $("#currentDay");
 let container = $(".container");
 let textArea = $("textarea");
 
-let todos = {
-  9: "",
-  10: "",
-  11: "",
-  12: "",
-  1: "",
-  2: "",
-  3: "",
-  4: "",
-  5: "",
-};
-
+//save button////////////////////////////
 container.on("click", "button", function (e) {
   e.preventDefault();
   //get name of the button
@@ -21,30 +10,42 @@ container.on("click", "button", function (e) {
   console.log(name);
   //connect button with text area of the same name
   let correctTextArea = $(`textarea[name = "${name}"]`);
-  //set storage
-  //get the todos object out of storage
-  let storagedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  //get out of storage or set as an object//////////
+  let storagedTodos = JSON.parse(localStorage.getItem("todos")) || {};
+
   //add todo to the properly number key in the todos object
   storagedTodos[name] = correctTextArea.val();
-
-  //loop through todos object
-  for (let todo in storagedTodos) {
-    //get key value then get text area
-    correctTextArea.text(storagedTodos[todo]);
-  }
+  //set back into local storage///////////
   localStorage.setItem("todos", JSON.stringify(storagedTodos));
 });
 
 todaysDateEl.text(moment().format("dddd MMMM Do"));
 
+// ---------------------------------------------------
+//init function///////////////////////////////////////
 function getStorage() {
   //get local storage
-  let storagedTodos = JSON.parse(localStorage.getItem("todos"));
-
+  let storagedTodos = JSON.parse(localStorage.getItem("todos")) || {};
+  //loop through todo object and place todos on textarea////
   for (let todo in storagedTodos) {
     let correctTextArea = $(`textArea[name="${todo}"`);
     correctTextArea.text(storagedTodos[todo]);
   }
 }
-
 getStorage();
+//set color coded time
+let currentHourNum = parseInt(moment().format("HH"));
+
+textArea.each((index) => {
+  let currIndex = $(textArea[index]);
+  let currIndexIdNum = parseInt(currIndex.attr("id"));
+
+  if (currIndexIdNum === currentHourNum) {
+    currIndex.addClass("present");
+  } else if (currIndexIdNum < currentHourNum) {
+    currIndex.addClass("past");
+  } else {
+    currIndex.addClass("future");
+  }
+});
